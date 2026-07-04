7d2ad494f518fcf867bdcfbd3bef8e09855becf2
Car <matijacarr@gmail.com>
2026-07-04 14:10:33 +0200

    Initial commit: empty Spring Boot project with Actuator

---
+++ src/main/java/com/actuator/ActuatorPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorPracticeApplication.class, args);
+    }
+
+}
