5e7cb7297868798cf7ecf7b8b640003ab6dc3567
Car <matijacarr@gmail.com>
2026-07-07 18:08:49 +0200

    Add custom InfoContributor and enable env info in actuator

---
+++ src/main/java/com/actuator/info/CustomInfoContributor.java
@@ -0,0 +1,15 @@
+package com.actuator.info;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomInfoContributor implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder.withDetail("customInfo", "This was added manually through code")
+                .withDetail("environment", "local-test");
+    }
+}
