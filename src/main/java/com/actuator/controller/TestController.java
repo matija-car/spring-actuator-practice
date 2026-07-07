8e04bf175c758f11b5be96af8096f1d216d77c56
Car <matijacarr@gmail.com>
2026-07-07 18:08:58 +0200

    Add custom Micrometer counter to /hello endpoint

--- src/main/java/com/actuator/controller/TestController.java
+++ src/main/java/com/actuator/controller/TestController.java
@@ -1,13 +1,22 @@
 package com.actuator.controller;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private final Counter helloRequestCounter;
+
+    public TestController(MeterRegistry registry) {
+        this.helloRequestCounter = registry.counter("hello.request.counter");
+    }
+
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello from test controller";
+    public String hello() {
+        helloRequestCounter.increment();
+        return "Hello from the test controller!";
     }
 }
